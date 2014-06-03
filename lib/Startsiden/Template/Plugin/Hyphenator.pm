5e0056d9bb8b9e203b3f1f2f6e5cfe77c9371132
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2014-06-03 12:41:15 +0200

    Use example from http://cpansearch.perl.org/src/ABW/Template-Toolkit-2.21/lib/Template/Plugin/Filter.pm and add filter method call parameters "delimiter" and "threshold"

--- lib/Startsiden/Template/Plugin/Hyphenator.pm
+++ lib/Startsiden/Template/Plugin/Hyphenator.pm
@@ -8,27 +8,81 @@ use Startsiden::Hyphenator;
 # This will die if Template::Toolkit is not in use, but then this plugin is not needed.
 use Template::Plugin::Filter;
 use parent qw( Template::Plugin::Filter );
+use Scalar::Util 'weaken';
+
+our $DYNAMIC = 0 unless defined $DYNAMIC;
+
+my $HAS_MOJO_DOM = eval { require Mojo::DOM; 1; };
+warn "Mojo::DOM not found, will not be able to hyphenate HTML" unless $HAS_MOJO_DOM;
 
 my $hyphenator;
 
-sub init {
-    my ($self) = @_;
-    $self->{has_mojo_dom} = eval { require Mojo::DOM; 1; };
-    warn "Mojo::DOM not found, will not be able to hyphenate HTML" unless $self->{has_mojo_dom};
-    $hyphenator ||= Startsiden::Hyphenator->new();
-    $self->install_filter('hyphen');
+sub new {
+    my ($class, $context, @args) = @_;
+    my $config = @args && ref $args[-1] eq 'HASH' ? pop(@args) : { };
 
+    # look for $DYNAMIC
+    my $dynamic;
+    {
+        no strict 'refs';
+        $dynamic = ${"$class\::DYNAMIC"};
+    }
+    $dynamic = $DYNAMIC unless defined $dynamic;
+
+    my $self = bless {
+        _CONTEXT => $context,
+        _DYNAMIC => $dynamic,
+        _ARGS    => \@args,
+        _CONFIG  => $config,
+    }, $class;
+
+    $hyphenator ||= Startsiden::Hyphenator->new();
+
+    return $self->init($config)
+        || $class->error($self->error());
+}
+
+
+sub init {
+    my ($self, $config) = @_;
+    my $name = $self->{ _CONFIG }->{ name } || 'hyphen';
+    $self->install_filter($name);
     return $self;
 }
 
+
+sub factory {
+    my $self = shift;
+    my $this = $self;
+    weaken($this);
+
+    if ($self->{ _DYNAMIC }) {
+        return $self->{ _DYNAMIC_FILTER } ||= [ sub {
+            my ($context, @args) = @_;
+            my $config = ref $args[-1] eq 'HASH' ? pop(@args) : { };
+        
+            return sub {
+                $this->filter(shift, \@args, $config);
+            };
+        }, 1 ];
+    }
+    else {
+        return $self->{ _STATIC_FILTER } ||= sub {
+            $this->filter(shift);
+        };
+    }
+}
+
 sub filter {
-    my ($self, $text) = @_;
+    my ($self, $text, $args, $config) = @_;
 
-    my $args      = $self->{_ARGS};
-    my $delim     = $args->[0] || "\x{00AD}";
-    my $threshold = $args->[1] || 10;
+    $args      = $self->merge_args($args);
+    $config    = $self->merge_config($config);
 
-    if ($self->{has_mojo_dom}) {
+    my $delim     = $args->[0] || $config->{delimiter} || "\x{00AD}";
+    my $threshold = $args->[1] || $config->{threshold} || 10;
+
+    if ($HAS_MOJO_DOM) {
         my $d = Mojo::DOM->new($text);
         $d->all_contents->map( sub {
             $_->content( $hyphenator->hyphenate($_->content, $delim, $threshold) ) if $_->node eq 'text';
@@ -38,6 +92,31 @@ sub filter {
     } else {
         return $hyphenator->hyphenate($text, $delim, $threshold);
     }
+
+    return $text;
+}
+
+
+sub merge_config {
+    my ($self, $newcfg) = @_;
+    my $owncfg = $self->{ _CONFIG };
+    return $owncfg unless $newcfg;
+    return { %$owncfg, %$newcfg };
+}
+
+
+sub merge_args {
+    my ($self, $newargs) = @_;
+    my $ownargs = $self->{ _ARGS };
+    return $ownargs unless $newargs;
+    return [ @$ownargs, @$newargs ];
+}
+
+
+sub install_filter {
+    my ($self, $name) = @_;
+    $self->{ _CONTEXT }->define_filter( $name => $self->factory() );
+    return $self;
 }
 
 1;
