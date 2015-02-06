f6658acc57b68397089d5ceaa1570b3df84d238b
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2015-02-06 13:58:56 +0100

    Add support for pre-generated tries in language modules

--- bin/gen-mod.pl
+++ bin/gen-mod.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
+use Data::Dumper;
 use Encode;
 use File::Basename;
 use File::Slurp;
 
-my ($language) = @ARGV;
+my ($language, $target_language, $pre_generate) = @ARGV;
 
 help() unless defined $language;
  
@@ -27,28 +31,75 @@ if ($content =~ m{ \\hyphenation \s* \{ \s* (.*?) \} }gmsx) {
   @exceptions = split /\s+/, $1;
 }
 
-print Encode::encode_utf8 "package Text::Hyphen::" . ucfirst($language) . ";
+use Text::Hyphen;
 
+*Text::Hyphen::_PATTERNS   = sub { [ map { $_ } @words ] };
+*Text::Hyphen::_EXCEPTIONS = sub { [ map { $_ } @exceptions ] };
+
+my $t = Text::Hyphen->new();
+$Data::Dumper::Indent = 0;
+my $PREGEN_TRIE = Dumper($t->{trie});
+{
+    no warnings 'uninitialized';
+    $PREGEN_TRIE =~ s{\s*|'([\w|\d+])'}{$1}gmx;
+};
+
+print Encode::encode_utf8 "package Text::Hyphen::" . ucfirst($target_language || $language) . ';
+# Generated with '."$0 @ARGV".'
 use strict;
 use warnings;
 
 use utf8;
-use base 'Text::Hyphen';
+use base q{Text::Hyphen};
 
-sub _PATTERNS {
-    return [qw(
-        @words
-    )];
+my $VAR1;
+';
+
+if ($pre_generate) {
+    print Encode::encode_utf8 $PREGEN_TRIE;
 }
 
+print Encode::encode_utf8 '
+sub new {
+    my ($proto, @fields) = @_;
+    my $class = ref $proto || $proto;
+
+    my $self = { @fields };
+    
+    $self->{min_word}   ||= 5;
+    $self->{min_prefix} ||= 2;
+    $self->{min_suffix} ||= 2;
+
+    $self->{trie} = $VAR1 || {};
+
+    bless $self, $class;
+
+    $VAR1 || $self->_load_patterns;
+
+    return $self;
+}
+
+';
+
+if ( ! $pre_generate ) {
+    print Encode::encode_utf8 '
+sub _PATTERNS {
+    return [qw(
+        ' . "@words" . '
+    )];
+}
+    ';
+}
+
+print Encode::encode_utf8 '
 sub _EXCEPTIONS {
     return [qw(
-        @exceptions
+        ' . "@exceptions" . '
     )];
 }
 
 1;
-";
+';
 
 sub get_tex_file {
    my ($language) = @_;
